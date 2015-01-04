3ec79a2b822d92644a4c7f9ff04c572a8222e604
qiuzhangzhi <happy_qzz@sina.com>
2015-01-04 23:16:20 +0800

    First commit

---
+++ src/com/grasp/thinker/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.grasp.thinker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by qiuzhangzhi on 15/1/4.
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
