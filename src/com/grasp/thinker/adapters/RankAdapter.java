5f4bcb7c6320b9499bec03be96c9744cff30fdfa
qiuzhangzhi <happy_qzz@sina.com>
2015-03-15 22:13:19 +0800

    排行榜

--- src/com/grasp/thinker/adapters/RankAdapter.java
+++ src/com/grasp/thinker/adapters/RankAdapter.java
@@ -1,29 +1,32 @@
 package com.grasp.thinker.adapters;
 
-import com.grasp.thinker.R;
-import com.grasp.thinker.model.Song;
-import com.grasp.thinker.utils.MusicUtils;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import com.grasp.thinker.R;
+import com.grasp.thinker.model.Song;
+import com.grasp.thinker.widgets.TextProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by qiuzhangzhi on 15/1/10.
+ * Created by qiuzhangzhi on 2015/3/15.
  */
-public class SongAdapter extends BaseAdapter {
+public class RankAdapter extends BaseAdapter {
+
+    private static final int INIT_PROGRESS_MAX = 50;
+
+    private int max  = INIT_PROGRESS_MAX;
 
     public Context mContext;
 
     public ArrayList<Song> mData = new ArrayList<Song>();
 
-    public SongAdapter(Context context){
+    public RankAdapter(Context context){
         mContext = context;
     }
 
@@ -49,19 +52,25 @@ public class SongAdapter extends BaseAdapter {
 
         if(convertView == null){
             viewHolder = new ViewHolder();
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.listview_item_songlist_adpter,null);
-            viewHolder.mSongName = (TextView)convertView.findViewById(R.id.song_name);
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.listview_item_ranklist_adapter,null);
             viewHolder.mSongInfo = (TextView)convertView.findViewById(R.id.artist_album);
-            viewHolder.mSongTime = (TextView)convertView.findViewById(R.id.song_time);
+            viewHolder.mSongProgress = (TextProgressBar)convertView.findViewById(R.id.rank_progress);
+            viewHolder.mProgressText = (TextView)convertView.findViewById(R.id.progress_text);
             convertView.setTag(viewHolder);
         }else {
             viewHolder = (ViewHolder)convertView.getTag();
         }
         Song song = getItem(position);
         if(song!=null){
-            viewHolder.mSongName.setText(song.mSongName);
-            viewHolder.mSongInfo.setText(String.format(mContext.getString(R.string.divider_artist_album),song.mArtistName,song.mAlbumName));
-            viewHolder.mSongTime.setText(""+ MusicUtils.makeTimeString(mContext,song.mDuration));
+            if(position == 0 && song.mPlaytimes >= INIT_PROGRESS_MAX){
+                max = 100 * (int)((song.mPlaytimes)/100 + 1);
+            }
+            viewHolder.mSongProgress.setMax(max);
+            viewHolder.mSongProgress.setIndicator(viewHolder.mProgressText);
+            viewHolder.mProgressText.setText("" + song.mPlaytimes);
+            viewHolder.mSongProgress.setProgress((int)song.mPlaytimes);
+            viewHolder.mSongInfo.setText(song.mSongName);
+
         }
         return convertView;
     }
@@ -73,8 +82,8 @@ public class SongAdapter extends BaseAdapter {
     }
 
     static class ViewHolder{
-        TextView mSongName;
         TextView mSongInfo;
-        TextView mSongTime;
+        TextProgressBar mSongProgress;
+        TextView mProgressText;
     }
 }
---
+++ src/com/grasp/thinker/widgets/TextProgressBar.java
@@ -0,0 +1,61 @@
+package com.grasp.thinker.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by qiuzhangzhi on 2015/3/15.
+ */
+public class TextProgressBar extends ProgressBar {
+
+    private TextView mText;
+
+    public TextProgressBar(Context context) {
+        super(context);
+    }
+
+    public TextProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TextProgressBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setIndicator(TextView text) {
+            mText = text;
+    }
+
+    @Override
+    public synchronized void setProgress(final int progress) {
+        if (mText != null) {
+           this.post(new Runnable() {
+               @Override
+               public void run() {
+                   float r = (5 * ((getMax() / (float)getWidth()))) ;
+
+                   float textPosition = ((getWidth() / (float)getMax()) * (progress+r)) + getLeft();
+                   int position = (int) Math.ceil(textPosition);
+
+                   setTextPositon(mText, position);
+               }
+           });
+        }
+        super.setProgress(progress);
+    }
+
+    private void setTextPositon(TextView v, int position) {
+        if (v != null) {
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) v
+                    .getLayoutParams();
+            layoutParams.leftMargin = position;
+            v.setLayoutParams(layoutParams);
+        }
+    }
+
+
+}
